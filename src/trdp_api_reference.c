8c549a1f9aef5ca2e870c5ea2e0184d1af26dd3f
Alok T J <aloktj97@gmail.com>
2025-11-25 01:06:19 +0530

    adding submodules, cmake and src main

--- src/trdp_api_reference.c
+++ src/trdp_api_reference.c
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 #include "iec61375-2-3.h"
 #include "tau_ctrl.h"
 #include "tau_ctrl_types.h"
@@ -15,74 +17,86 @@
 
 int main(void)
 {
-    TRDP_MEM_CONFIG_T memConfig = {NULL, 0u, {0}};
+    /* Instantiate common configuration and type structures to ensure availability. */
+    TRDP_MEM_CONFIG_T memConfig = {0};
     TRDP_PROCESS_CONFIG_T processConfig = {0};
     TRDP_APP_SESSION_T session = NULL;
 
-    TRDP_ERR_T err = tlc_init(NULL, NULL, &memConfig);
-    err = tlc_openSession(&session, 0u, 0u, NULL, NULL, NULL, &processConfig);
-
-    (void)tlc_getOwnIpAddress(session);
-    TRDP_TIME_T interval = {0};
-    TRDP_FDS_T fds = {0};
-    TRDP_SOCK_T numDesc = 0;
-    (void)tlc_getInterval(session, &interval, &fds, &numDesc);
-
-    /* IEC 61375 definitions and base TRDP types */
-    UINT16 pdPort = TRDP_PD_UDP_PORT;
     TRDP_PD_CONFIG_T pdConfig = {0};
     TRDP_MD_CONFIG_T mdConfig = {0};
     TRDP_PD_INFO_T pdInfo = {0};
+    TRDP_TIME_T interval = {0};
+    TRDP_FDS_T fds = {0};
+    TRDP_SOCK_T numDesc = 0;
 
     /* tau_ctrl_types / tau_ctrl */
     TRDP_ECSP_CTRL_T ctrlInfo = {0};
-    (void)tau_initEcspCtrl(session, 0u);
-    (void)tau_setEcspCtrl(session, &ctrlInfo);
 
     /* tau_dnr_types / tau_dnr */
     TRDP_DNS_REQUEST_T dnsRequest = {0};
-    (void)tau_initDnr(session, 0u, TRDP_MD_UDP_PORT, NULL, TRDP_DNR_COMMON_THREAD, FALSE);
-    (void)tau_DNRstatus(session);
 
     /* tau_marshall */
     void *marshallContext = NULL;
-    (void)tau_initMarshall(&marshallContext, 0u, NULL, 0u, NULL);
     TRDP_DATASET_T *cachedDataset = NULL;
     UINT32 destSize = 0;
-    (void)tau_marshall(marshallContext, 0u, NULL, 0u, NULL, &destSize, &cachedDataset);
 
     /* tau_so_if */
     SRM_SERVICE_INFO_T serviceInfo = {0};
-    (void)tau_addService(session, &serviceInfo, FALSE);
 
     /* tau_tti_types / tau_tti */
     TRDP_OP_TRAIN_DIR_STATUS_INFO_T statusInfo = {0};
     UINT16 consistCount = 0;
-    (void)tau_getOpTrnDirectoryStatusInfo(session, &statusInfo);
-    (void)tau_getTrnCstCnt(session, &consistCount);
 
     /* tau_xml */
     TRDP_XML_DOC_HANDLE_T xmlHandle = {0};
-    (void)tau_prepareXmlDoc(NULL, &xmlHandle);
-    (void)tau_freeXmlDoc(&xmlHandle);
 
-    /* trdp_serviceRegistry */
+    /* IEC 61375 definitions and other TRDP constants */
+    UINT16 pdPort = TRDP_PD_UDP_PORT;
     UINT8 serviceRegistryFlag = TRDP_SR_FLAG_EVENT;
     UINT32 srmComId = SRM_SERVICE_READ_REQ_COMID;
-
-    /* trdp_tsn_def */
     UINT8 tsnMessage = TRDP_MSG_TSN_PD;
 
-    /* tau_dnr_types use */
-    TCN_URI_T uriEntry = {0};
+    /* Reference at least one API symbol from each header to validate linkage. */
+    static volatile void *symbol_refs[] = {
+        /* trdp_if_light.h */
+        (void *)&tlc_init,
+        (void *)&tlc_terminate,
+        (void *)&tlc_openSession,
+        (void *)&tlc_closeSession,
+        (void *)&tlc_getOwnIpAddress,
+        (void *)&tlc_getInterval,
+        /* tau_ctrl.h */
+        (void *)&tau_initEcspCtrl,
+        (void *)&tau_setEcspCtrl,
+        /* tau_dnr.h */
+        (void *)&tau_initDnr,
+        (void *)&tau_DNRstatus,
+        /* tau_marshall.h */
+        (void *)&tau_initMarshall,
+        (void *)&tau_marshall,
+        /* tau_so_if.h */
+        (void *)&tau_addService,
+        /* tau_tti.h */
+        (void *)&tau_getOpTrnDirectoryStatusInfo,
+        (void *)&tau_getTrnCstCnt,
+        /* tau_xml.h */
+        (void *)&tau_prepareXmlDoc,
+        (void *)&tau_freeXmlDoc
+    };
 
-    /* Aggregate values to keep the compiler happy */
-    UINT32 aggregate = pdPort + serviceRegistryFlag + tsnMessage + TRDP_MAX_CST_CNT +
+    size_t live_symbols = 0;
+    for (size_t i = 0; i < (sizeof(symbol_refs) / sizeof(symbol_refs[0])); ++i)
+    {
+        live_symbols += (symbol_refs[i] != NULL);
+    }
+
+    /* Aggregate values to keep the compiler/linker from discarding references. */
+    UINT32 aggregate = (UINT32)live_symbols + pdPort + serviceRegistryFlag + tsnMessage + TRDP_MAX_CST_CNT +
                        ctrlInfo.leadVehOfCst + dnsRequest.tcnUriCnt + statusInfo.state.opTrnDirState +
                        consistCount + srmComId + pdConfig.timeout + mdConfig.replyTimeout + pdInfo.comId +
-                       uriEntry.tcnUriIpAddr;
-    (void)aggregate;
+                       interval.tv_usec + (UINT32)sizeof(fds) + numDesc + (UINT32)(size_t)marshallContext + destSize +
+                       (UINT32)(size_t)cachedDataset + (UINT32)(size_t)serviceInfo.serviceId +
+                       (UINT32)(size_t)xmlHandle.pXmlDocument + memConfig.size + processConfig.options;
 
-    (void)tlc_terminate();
-    return (err == TRDP_NO_ERR) ? 0 : 1;
+    return (aggregate != 0u || session != NULL) ? 0 : 1;
 }
